1b2396e6be554bffe15c0fe6a3b9c4a4eeb3e08f
Guido Mastropasqua <guidomastropasqua@gmail.com>
2025-02-13 18:51:37 -0300

    primer subida al repo

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import NavBar from './components/NavBar/NavBar'
+import Card from './components/Card/Card'
+
+function App() {
+
+  return (
+    <>
+      <NavBar />
+      <Card />
+      <Card />
+
+    </>
+  )
+}
+
+export default App
